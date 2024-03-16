66299548b7e30fcf58ecd3b5b582047bcff9f847
IceLightningStrike <german.542@mail.ru>
2024-03-16 18:38:58 +0300

    A few changes

--- main.py
+++ main.py
@@ -103,15 +103,21 @@ def game_field() -> str:
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+
     if form.validate_on_submit():
         db_sess = db_session.create_session()
         user = db_sess.query(User).filter(User.email == form.email.data).first()
+
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
             return redirect("/")
-        return render_template('login.html',
-                               message="Неправильный логин или пароль",
-                               form=form)
+        
+        return render_template(
+            'login.html',
+            message="Неправильный логин или пароль",
+            form=form
+        )
+    
     return render_template('login.html', title='Авторизация', form=form)
 
 
@@ -120,23 +126,33 @@ def reqister():
     form = RegisterForm()
     if form.validate_on_submit():
         if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Пароли не совпадают")
+            return render_template(
+                'register.html',
+                title='Регистрация',
+                form=form,
+                message="Пароли не совпадают"
+            )
+        
         db_sess = db_session.create_session()
         if db_sess.query(User).filter(User.email == form.email.data).first():
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Такой пользователь уже есть")
+            return render_template(
+                'register.html', title='Регистрация',
+                form=form,
+                message="Такой пользователь уже есть"
+            )
+        
         user = User(
             name=form.name.data,
             email=form.email.data,
             about=form.about.data
         )
+
         user.set_password(form.password.data)
         db_sess.add(user)
         db_sess.commit()
+
         return redirect('/login')
+
     return render_template('register.html', title='Регистрация', form=form)
 
 
